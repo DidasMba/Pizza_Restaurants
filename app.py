b533d851cc2b085555ddc9b716f98b763f72e0e4
DidasMba <daniel.mbarushi@gmail.com>
2024-01-21 11:29:05 +0300

    updateflaskapp

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 # app.py
-# app.py
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  # Import Flask-Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # Initialize Flask-Migrate
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
