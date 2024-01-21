08ee567268e7b515a251438869dcfb29a02494bc
DidasMba <daniel.mbarushi@gmail.com>
2024-01-21 11:24:04 +0300

    checkup

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # app.py
+# app.py
 
 from flask import Flask
-from  flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+
 
