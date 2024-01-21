08df72378e9e8ec0e0a93496036a4fb239b53b59
DidasMba <daniel.mbarushi@gmail.com>
2024-01-21 12:13:38 +0300

    aditmodels

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 # app.py
+# app.py
+# app.py
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate  # Import Flask-Migrate
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello word'
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)  # Initialize Flask-Migrate
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,9 +2,21 @@
 
 from app import db
 
-class User(db.Model):
+class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    ingredients = db.Column(db.String(255), nullable=False)
 
-    def __repr__(self):
-        return f"User('{self.username}')"
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+
+    pizza = db.relationship('Pizza', backref='restaurant_pizzas')
+    restaurant = db.relationship('Restaurant', backref='restaurant_pizzas')
